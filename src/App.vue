18bbd71df7dac5626510ec49aabd0c4b40636550
Peashooter101 <Peashooter101@yahoo.com>
2023-03-24 12:16:53 -0700

    Messing with Selecting Agents and Pulling Assets
    ¯\_(ツ)_/¯
    
    Lots of errors of failing to resolve stuff cause obviously, stuff is missing until user chooses an agent.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
   <AgentRoulette/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import AgentRoulette from './components/AgentRoulette.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     AgentRoulette
   }
 }
--- src/components/AgentRoulette.vue
+++ src/components/AgentRoulette.vue
@@ -1,25 +1,90 @@
 <template>
-    <p v-if="agentsByName">{{ agentsByName }}</p>
-    <p v-else>hi</p>
+    <div v-if="agentsByName.size != 0">
+        <select v-model="selectedAgent">
+            <option value="" selected disabled>Choose Agent</option>
+            <option v-for="agent in agentsByName.keys()" :key="agent">{{ agent }}</option>
+        </select>
+        <div v-if="selectedAgent">
+            <h1>{{ selectedAgent }}</h1>
+            <div class="agent-info">
+                <img class="agent-icon" :src="selectedAgentJson.displayIcon"/>
+                <div class="agent-details">
+                    <p>{{ selectedAgentJson.description }}</p><br/>
+                    <div class="ability-list">
+                        <img v-for="ability in selectedAgentJson.abilities" :key="ability" class="ability-icon" :src="ability.displayIcon || 'https://static.wikia.nocookie.net/overwatch_gamepedia/images/5/53/Ability-genji5.png'"/>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1>Debug Information</h1>
+                <p>Selected Agent: {{ selectedAgent }}</p>
+                <p>Agent displayIcon: {{ selectedAgentJson.displayIcon }}</p>
+                <p>Agent JSON: {{ selectedAgentJson }}</p>
+            </div>
+        </div>
+    </div>
+    <p v-else>Agents are not yet loaded.</p>
 </template>
 
 <script setup>
-    /* eslint-disable */
-    import { ref } from 'vue';
+    import { watch, ref } from 'vue';
     const agentsEndpoint = "https://valorant-api.com/v1/agents";
     const agentsByName = ref(new Map());
-    setTimeout(async () => await parseAgents(), 5000);
+    const selectedAgent = ref('');
+    const selectedAgentJson = ref(null);
+    parseAgents()
 
     async function parseAgents() {
         fetch(agentsEndpoint + "?isPlayableCharacter=true")
-            .then(async r => {
-                var agentsJson = JSON.parse(await r.text()).data;
+            .then(async response => {
+                var agentsJson = JSON.parse(await response.text()).data;
                 agentsJson.forEach((i) => agentsByName.value.set(i.displayName, i.uuid));
             });
-        console.log(agentsByName.value);
     }
+
+    watch(selectedAgent, (newValue) => {
+        fetch(agentsEndpoint + "/" + agentsByName.value.get(newValue)).then(
+            async response => {
+                selectedAgentJson.value = JSON.parse(await response.text()).data;
+            })
+    })
 </script>
 
 <style scoped>
+    img.agent-icon {
+        width: 200px;
+        height: 200px;
+    }
+    img.ability-icon {
+        width: 100px;
+        height: 100px;
+        margin: 10px;
+    }
 
+    .agent-info {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .agent-details {
+        min-width: 500px;
+        max-width: 35%;
+    }
+
+    p.agent-details {
+        word-break: normal;
+        white-space: normal;
+    }
+
+    .ability-list {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: space-around;
+        background-color: black;
+        padding:10px;
+    }
 </style>
